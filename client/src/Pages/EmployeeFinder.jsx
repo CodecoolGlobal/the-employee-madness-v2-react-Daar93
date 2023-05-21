e916a7fac3e4cc7901932a0c86b8e361ca6ec8a0
Mehmet <mehmet-ali-yilmaz@live.com>
2023-05-21 21:04:43 +0200

    client: create new /employee/:search route

---
+++ client/src/Pages/EmployeeFinder.jsx
@@ -0,0 +1,4 @@
+
+export default function EmployeeFinder() {
+    return <>Halloooo</>
+};
--- client/src/index.js
+++ client/src/index.js
@@ -11,6 +11,7 @@ import EmployeeUpdater from "./Pages/EmployeeUpdater";
 import EquipmentList from "./Pages/EquipmentList";
 import EquipmentCreator from "./Pages/EquipmentCreator";
 import EquipmentUpdater from "./Pages/EquipmentUpdater";
+import EmployeeFinder from "./Pages/EmployeeFinder.jsx";
 
 import "./index.css";
 import TableTest from "./Pages/TableTest";
@@ -53,6 +54,10 @@ const router = createBrowserRouter([
       {
         path: "/update/equipments/:id",
         element: <EquipmentUpdater />
+      },
+      {
+        path: "/employee/:search",
+        element: <EmployeeFinder />
       }
     ],
   },
