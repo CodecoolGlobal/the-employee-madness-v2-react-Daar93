84880481dd7c80f2bc140004f76c0d34d7e16965
Mehmet <mehmet-ali-yilmaz@live.com>
2023-05-21 21:20:36 +0200

    client: added fetch to search for names

--- client/src/Pages/EmployeeFinder.jsx
+++ client/src/Pages/EmployeeFinder.jsx
@@ -1,4 +1,23 @@
+import { useEffect, useState } from "react";
+
 
 export default function EmployeeFinder() {
+    const [foundEmployee, setFoundEmployees] = useState(null);
+
+    useEffect(() => {
+        console.log(window.location.href.lastIndexOf("/"));
+        const nameToSearch = window.location.href;
+        const searchValue = nameToSearch.slice(nameToSearch.lastIndexOf("/") + 1);
+        console.log(searchValue);
+
+        console.log(searchValue)
+        fetch("http://localhost:8080/employees/" + searchValue)
+            .then(data => data.json())
+            .then(employees => {
+                console.log(employees);
+            })
+
+    }, []);
+
     return <>Halloooo</>
 };
